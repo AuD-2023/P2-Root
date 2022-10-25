dde54c05fb64b1dab84bdcdd9e9947062fc7d32e
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-25 14:11:13 +0200

    Update template to Jagr v0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,30 +3,38 @@ plugins {
     java
     application
     alias(libs.plugins.style)
-    alias(libs.plugins.submitter)
+    alias(libs.plugins.jagr.gradle)
 }
 
 version = file("version").readLines().first()
 
-submit {
-    assignmentId = "h_id_"
-    studentId = "ab12cdef"
-    firstName = "sol_first"
-    lastName = "sol_last"
-    requireTests = false
-}
-
-val grader: SourceSet by sourceSets.creating {
-    val test = sourceSets.test.get()
-    compileClasspath += test.output + test.compileClasspath
-    runtimeClasspath += output + test.runtimeClasspath
+jagr {
+    assignmentId.set("h_id_")
+    submissions {
+        val main by creating {
+            studentId.set("ab12cdef")
+            firstName.set("sol_first")
+            lastName.set("sol_last")
+        }
+    }
+    graders {
+        val graderPublic by creating {
+            graderName.set("_name_-Public")
+            rubricProviderName.set("h_id_.H_id__RubricProvider")
+            configureDependencies {
+                implementation(libs.algoutils.tutor)
+            }
+        }
+        val graderPrivate by creating {
+            parent(graderPublic)
+            graderName.set("_name_-Private")
+        }
+    }
 }
 
 dependencies {
     implementation(libs.annotations)
-    "graderCompileOnly"(libs.jagr.launcher) {
-        exclude("org.jetbrains", "annotations")
-    }
+    implementation(libs.algoutils.student)
     testImplementation(libs.junit.core)
 }
 
@@ -36,7 +44,7 @@ application {
 
 tasks {
     val runDir = File("build/run")
-    named<JavaExec>("run") {
+    withType<JavaExec> {
         doFirst {
             runDir.mkdirs()
         }
@@ -49,62 +57,9 @@ tasks {
         workingDir = runDir
         useJUnitPlatform()
     }
-    val graderTest by creating(Test::class) {
-        group = "verification"
-        doFirst {
-            runDir.mkdirs()
-        }
-        workingDir = runDir
-        testClassesDirs = grader.output.classesDirs
-        classpath = grader.compileClasspath + grader.runtimeClasspath
-        useJUnitPlatform()
-    }
-    named("check") {
-        dependsOn(graderTest)
-    }
-    val graderJar by creating(Jar::class) {
-        group = "build"
-        afterEvaluate {
-            archiveFileName.set("_name_-${project.version}.jar")
-            from(sourceSets.main.get().allSource)
-            from(sourceSets.test.get().allSource)
-            from(grader.allSource)
-        }
-    }
-    val graderLibs by creating(Jar::class) {
-        group = "build"
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-        // don't include Jagr's runtime dependencies
-        val jagrRuntime = configurations["graderCompileClasspath"]
-            .resolvedConfiguration
-            .firstLevelModuleDependencies
-            .first { it.moduleGroup == "org.sourcegrade" && it.moduleName == "jagr-launcher" }
-            .allModuleArtifacts
-            .map { it.file }
-
-        val runtimeDeps = grader.runtimeClasspath.mapNotNull {
-            if (it.path.toLowerCase().contains("h_id_") || jagrRuntime.contains(it)) {
-                null
-            } else if (it.isDirectory) {
-                it
-            } else {
-                zipTree(it)
-            }
-        }
-        from(runtimeDeps)
-        archiveFileName.set("_name_-${project.version}-libs.jar")
-    }
-    create("graderAll") {
-        group = "build"
-        dependsOn(graderJar, graderLibs)
-    }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
         sourceCompatibility = "17"
         targetCompatibility = "17"
-    }
-    jar {
-        enabled = false
     }
 }
